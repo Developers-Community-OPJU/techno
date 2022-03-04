20e0c3c5599629d78ffb7cd71c0fd185b49e11c2
Aman Vishwakarma <amankumar430206@gmail.com>
2022-03-04 23:08:28 +0530

    evenet page layout added

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import EventView from "../views/EventView.vue";
+import EventDetail from "../components/events/EventDetail.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+  },
+  {
+    path: "/events",
+    name: "events",
+    component: EventView,
+  },
+  {
+    path: "/events/:eventName",
+    name: "eventDetail",
+    component: EventDetail,
   },
   {
     path: "/about",

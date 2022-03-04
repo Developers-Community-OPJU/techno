b5afd05b6ceffcb49277bf5cca7cbcbbf25544f4
subrat k jena <subratj611@gmail.com>
2022-03-04 23:47:28 +0530

    preloader added

---
+++ src/views/PreLoad.vue
@@ -0,0 +1,25 @@
+<template>
+  <video
+    id="preloader"
+    src="../assets/mixkit-123-preview.mp4"
+    autoplay
+    muted
+  ></video>
+</template>
+
+<script>
+// const preloader = document.getElementById("preloader");
+
+export default {
+  name: "PreLoader",
+};
+</script>
+
+<style>
+#preloader {
+  position: fixed;
+  min-width: 100%;
+  min-height: 100%;
+  z-index: 1;
+}
+</style>

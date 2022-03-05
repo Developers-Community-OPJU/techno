68df462de0463fcb08f05c934bf224a9849be193
subrat k jena <subratj611@gmail.com>
2022-03-05 11:05:29 +0530

    event page complete

--- src/components/events/EventDetail.vue
+++ src/components/events/EventDetail.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="event-detail">
     <!-- your content here -->
-    <div class="overlay"></div>
     <div class="event-box">
       <div class="event__content">
-        <h1>Event : {{ $route.params.eventName }}</h1>
+        <h1>{{ $route.params.eventName }}</h1>
         <p>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus,
           voluptates? Incidunt explicabo delectus, vel maiores eligendi
@@ -14,41 +13,45 @@
           nesciunt! Tempore minima autem ullam aperiam distinctio aspernatur
           cupiditate at!
         </p>
+        <div class="rules">
+          <h4>Rules</h4>
+          <ul>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat,
+              quisquam.
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat,
+              quisquam.
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat,
+              quisquam.
+            </li>
+          </ul>
+        </div>
+        <h4 class="title">Team</h4>
+        <div class="team">
+          <div class="member">
+            <img src="@/assets/user.jpg" alt="" />
+            <h5 class="">Aman Vishwakarma</h5>
+            <p>B.tech CSE 8</p>
+          </div>
+          <div class="member">
+            <img src="@/assets/user.jpg" alt="" />
+            <h5 class="">Aman Vishwakarma</h5>
+            <p>B.tech CSE 8</p>
+          </div>
+          <div class="member">
+            <img src="@/assets/user.jpg" alt="" />
+            <h5 class="">Aman Vishwakarma</h5>
+            <p>B.tech CSE 8</p>
+          </div>
+        </div>
       </div>
       <div class="event__links">
         <a href="#" class="registration">Registration</a>
         <a href="#" class="more">More</a>
-      </div>
-    </div>
-    <div class="event-rule">
-      <h1>Rules</h1>
-      <div class="border__line">
-        <ul>
-          <li>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Voluptatem, obcaecati.
-          </li>
-          <li>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Voluptatem, obcaecati.
-          </li>
-          <li>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Voluptatem, obcaecati.
-          </li>
-          <li>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Voluptatem, obcaecati.
-          </li>
-          <li>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Voluptatem, obcaecati.
-          </li>
-          <li>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Voluptatem, obcaecati.
-          </li>
-        </ul>
       </div>
     </div>
   </div>
@@ -65,51 +68,66 @@ export default {};
   box-sizing: border-box;
   z-index: 1;
 }
+.title {
+  font-size: 1rem;
+  margin: 2rem 0 1rem 0;
+}
+h1 {
+  text-transform: capitalize;
+}
+.rules {
+  h4 {
+    margin: 2rem 0 1rem 0;
+  }
+  ul {
+    list-style: none;
+    li {
+      margin: 0.25rem 0;
+    }
+  }
+}
+.team {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 2rem 0;
+  .member {
+    margin-right: 2rem;
+    img {
+      height: 80px;
+      width: 80px;
+      border-radius: 50%;
+      filter: grayscale(100%) sepia(0%);
+      transition: all 0.5s ease;
+      &:hover {
+        filter: grayscale(0%) sepia(100%);
+      }
+    }
+    p {
+      font-size: 12px;
+    }
+    text-align: center;
+    display: inline-block;
+  }
+}
 .event-detail {
-  // background-image: linear-gradient(
-  //   to right top,
-  //   #051937,
-  //   #3c225b,
-  //   #811669,
-  //   #c4005a,
-  //   #f21330
-  // );
-  background-image: url("../../assets/party.jpg");
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
+  background-image: linear-gradient(to bottom, #061d3f, #111111);
   height: 100vh;
   color: #fff;
   position: relative;
   overflow: auto;
 
-  .overlay {
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 1980px;
-    width: 100%;
-    background-color: rgba(0, 0, 0, 0.45);
-    z-index: -1;
-  }
-
   .event-box {
-    box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
-    border-radius: 0.5rem;
-    border-top: 1px solid rgba(255, 255, 255, 0.2);
-    border-left: 1px solid rgba(255, 255, 255, 0.1);
-    backdrop-filter: blur(7px);
-    height: 80%;
+    height: 100%;
     width: 90%;
     min-width: 300px;
     display: flex;
     justify-content: space-around;
-    align-items: center;
-    margin: 3% auto;
+    align-items: flex-start;
+    flex-wrap: wrap;
+    margin: 3rem auto;
+    padding: 4.5rem 0;
 
     .event__content {
-      // width: 500px;
-      // padding: 0 ;
       & > p {
         width: 500px;
         padding-top: 2rem;
@@ -121,13 +139,13 @@ export default {};
       flex-direction: column;
       & .registration,
       a {
-        margin: 0.2rem 0;
+        margin: 0.45rem 0;
         text-decoration: none;
         color: #051937;
         font-size: 1.1rem;
         font-weight: 500;
         padding: 0.8em;
-        border-radius: 7px;
+        border-radius: 5px;
         background-color: #a8eb12;
         box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
         border-top: 1px solid rgba(255, 255, 255, 0.2);
@@ -138,48 +156,6 @@ export default {};
         background-color: rgba(255, 255, 255, 0.2);
         color: #fff;
         backdrop-filter: blur(5px);
-      }
-    }
-  }
-  .event-rule {
-    width: 90%;
-    margin: 0 auto;
-    h1 {
-      text-align: center;
-      font-size: 3rem;
-      margin: 13% 0 5% 0;
-      color: #fdd819;
-      position: relative;
-    }
-    .border__line {
-      border-left: 1px solid #fff;
-      ul {
-        li {
-          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
-          // background-color: rgba(85, 155, 247, 0.85);
-          background-image: linear-gradient(to right, #4facfe 0%, #00f2fe 100%);
-          border: 2px solid #006eee;
-          border-radius: 4px;
-          margin: 2rem 1rem;
-          padding: 1%;
-          color: #051937;
-          font-weight: 500;
-          width: fit-content;
-          list-style: none;
-          cursor: pointer;
-          &::before {
-            content: "\2022";
-            color: #46aef7;
-            // background-image: radial-gradient(circle 248px at center, #16d9e3 0%, #30c7ec 47%, #46aef7 100%);
-            width: 1.4rem;
-            display: inline-block;
-            margin-left: -1.8rem;
-            font-weight: bolder;
-          }
-          &:hover {
-            transform: scaleX(105%) translateX(20px);
-          }
-        }
       }
     }
   }

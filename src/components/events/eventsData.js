426999d900169fa9a4179ba5b1dfe84271df5ca5
Aman Vishwakarma <amankumar430206@gmail.com>
2022-03-06 20:03:38 +0530

    added eventData.json

---
+++ src/components/events/eventsData.js
@@ -0,0 +1,105 @@
+const eventsData = [
+  {
+    name: "Tech lab",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+        neque libero molestias laborum deserunt! Eius `,
+    link: "",
+  },
+  {
+    name: "tech docx",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+        neque libero molestias laborum deserunt! Eius.`,
+  },
+  {
+    name: "event name",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+        neque libero molestias laborum deserunt! Eius.`,
+  },
+  {
+    name: "event name",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+        neque libero molestias laborum deserunt! Eius.`,
+  },
+  {
+    name: "event name",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+        neque libero molestias laborum deserunt! Eius.`,
+  },
+  {
+    name: "event name",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+        neque libero molestias laborum deserunt! Eius.`,
+  },
+  {
+    name: "event name",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+        neque libero molestias laborum deserunt! Eius.`,
+  },
+  {
+    name: "event name",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+        neque libero molestias laborum deserunt! Eius.`,
+  },
+  {
+    name: "event name",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+        neque libero molestias laborum deserunt! Eius.`,
+  },
+  {
+    name: "event name",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+        neque libero molestias laborum deserunt! Eius.`,
+  },
+  {
+    name: "event name",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+        neque libero molestias laborum deserunt! Eius.`,
+  },
+  {
+    name: "event name",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+        neque libero molestias laborum deserunt! Eius.`,
+  },
+  {
+    name: "event name",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+        neque libero molestias laborum deserunt! Eius.`,
+  },
+  {
+    name: "event name",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+        neque libero molestias laborum deserunt! Eius.`,
+  },
+  {
+    name: "event name",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+        neque libero molestias laborum deserunt! Eius.`,
+  },
+  {
+    name: "event name",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+        neque libero molestias laborum deserunt! Eius.`,
+  },
+  {
+    name: "event name",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+        neque libero molestias laborum deserunt! Eius.`,
+  },
+  {
+    name: "event name",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+        neque libero molestias laborum deserunt! Eius.`,
+  },
+  {
+    name: "event name",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+        neque libero molestias laborum deserunt! Eius.`,
+  },
+  {
+    name: "event name",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+        neque libero molestias laborum deserunt! Eius.`,
+  },
+];
+
+export default eventsData;

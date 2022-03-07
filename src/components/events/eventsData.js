a658fca1e010d4189e611d39ac4eff21f5fc7360
Aman Vishwakarma <amankumar430206@gmail.com>
2022-03-08 03:14:48 +0530

    events data added

--- src/components/events/eventsData.js
+++ src/components/events/eventsData.js
@@ -1,8 +1,8 @@
 const eventsData = [
   {
     name: "Tech lab",
-    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
-        neque libero molestias laborum deserunt! Eius `,
+    desc: `Tech lab team feels proud to announce that your models are going to be examine by
+    our HONOURABLE GUEST: EXECUTIVE DIRECTORATE OF NATIONAL THERMAL POWER CORPORATION, LARA(NTPC). `,
     link: "",
     faqs: [
       {
@@ -21,6 +21,354 @@ const eventsData = [
         name: "",
         contact: "",
         designation: "",
+      },
+    ],
+  },
+  {
+    name: "Tech Docx",
+    desc: `The word TechDocx stands for a new idea innovation and invention. We budding engineers are today’s inventors, innovators and thinkers. So get your chance to be a part of the event TECH.DOCX and come forward to present your ideas and enhance the competencies through paper presentation. `,
+    link: "",
+    faqs: [
+      {
+        question: "",
+        answer: "",
+      },
+      {
+        question: "",
+        answer: "",
+      },
+    ],
+    rules: [
+      "The maximum word limit for the abstract is 350 words. ",
+      "Submit the soft copy of abstract on email ID tanmayvatsa1507@gmail.com before the last date.",
+      "All the abstracts should be in the prescribed format only.",
+      "All the abstracts should be submitted by means of email only.",
+      "Write all the necessary details of the authors (students) in the research paper.",
+      "Maximum number of authors (students) per paper is three.",
+      "Maximum time for each paper is 10 minutes and 2 minutes for the question and answers.",
+      "Presentation should be in the form of ppt only.",
+      "The abstracts will undergo screening and only the selected abstracts will be given a chance for presentation.",
+      "The participants will be informed through mail regarding the abstract selection.",
+      "In case of selection the participants will be sent a confirmation mail.",
+      "Participants are requested to mention the names of all the team members along with their contact numbers in the abstract.",
+      "Participants should also submit a hardcopy of the report to the respective coordinators after the presentation is over.",
+      "At the time of registration the selected teams will be provided a registration number which will be used for future reference.",
+      "All the participants presence is must at the time of presentation in the particular venue and they are expected to answer the questions as asked by the judges.",
+      "The decision of the judges shall be treated as final.",
+      "Accommodation will be provided on prior information.",
+      "All participants must carry their identity cards and registration slip at the time of presentation.",
+    ],
+    timeline: "",
+    team: [
+      {
+        name: "",
+        contact: "",
+        designation: "",
+      },
+    ],
+  },
+  {
+    name: "Robovation",
+    desc: `Construct Your Machine to have the maximum speed to beat other machines on the given track to reach the final destination in minimum time. You have to build a manually controlled robot capable of traversing over different terrain and hurdles. There will be separate points for overcoming the obstacles as well as the other parameter being within time. Thus, the bot with the most cumulative points will win. The aim of the event is to check the robustness of the vehicle and its competence with the opponent on the same track. `,
+    link: "",
+    faqs: [
+      {
+        question: "",
+        answer: "",
+      },
+      {
+        question: "",
+        answer: "",
+      },
+    ],
+    rules: ["Rule 1 ", "rule 2"],
+    timeline: "",
+    team: [
+      {
+        name: "",
+        contact: "",
+        designation: "",
+      },
+    ],
+  },
+  {
+    name: "Exhibiton and Expo - Photorollix",
+    desc: `In this event participants need to click a selfie of their own based on our themes and judges will evaluate according to set criteria. 
+    The themes will be divided into various parts depicting  rounds:
+     The themes will be decided later on.
+     •	Based on ourthemes, all entries will judged and after that the best 50% entries will move to the next round.
+     •	And best 3 will be awarded, also 2 consolation prices. 
+`,
+    link: "",
+    faqs: [
+      {
+        question: "",
+        answer: "",
+      },
+      {
+        question: "",
+        answer: "",
+      },
+    ],
+    rules: [
+      "All the entries must be submitted in google form rather than sending privately. ",
+      "You strictly have to mention the Messageand Locationof your clicked snap in Google form.",
+      "Register only once in google form. Twice registering will be subjected to disqualification also. Do mention all your details correctly.",
+      "The points will be given in 2 parts 50% will be given by our judge and rest 50% will be on basis of like comments and social engagements.",
+      "If participants have their Instagramid then they have to follow our page before sending their entries.",
+      "If your entry is selected then we will personally mail you for the next round.",
+      "The prizes and certificates will be directly given from college on the Prize Distribution Day.",
+      "For further details contact: Event Managers & Faculty Coordinator.",
+    ],
+    timeline: "",
+    team: [
+      {
+        name: "Nikhil Nivedan Vatsa (Meta, 8th Sem) ",
+        contact: "nivats@opju.ac.in, 8298071805",
+        designation: "Event Managers",
+      },
+      {
+        name: "Amul Raj (Meta, 8th Sem)  ",
+        contact: "amraj@opju.ac.in, 95751 79658",
+        designation: "Event Managers",
+      },
+      {
+        name: "Vedaj Patel",
+        contact: "",
+        designation: "Coordinater",
+      },
+    ],
+  },
+  {
+    name: "Exhibiton and Expo - Glass Painting",
+    desc: `Glass Painting is a type of art where we need to paint over the glass and this event contain only single round.This event is a theme based event and the theme is Festivals`,
+    link: "",
+    faqs: [
+      {
+        question: "",
+        answer: "",
+      },
+      {
+        question: "",
+        answer: "",
+      },
+    ],
+    rules: [
+      "You need to bring all the material you need except glass.",
+      "No plagiarism, otherwise you will be disqualified.",
+      "Use of Mobile phones or any other electronic devices are prohibited during this event.",
+      "Only 10 mins will be given to arrange your materials.",
+      "Registration is only valid through google forms.",
+      "You can either use fabric paint or glass paint.",
+      "The time duration of the event is 1hr 30mins.",
+    ],
+    timeline: "",
+    team: [
+      {
+        name: "Simrella Agrawal (CSE, 6th Sem) ",
+        contact: "7489211170",
+        designation: "Event Manager",
+      },
+    ],
+  },
+  {
+    name: "Exhibiton and Expo - Face Painting",
+    desc: `Face Painting is a type of art where we need to paint over the face and this event contain only single round.This event is a theme based event and the theme are Hallowen and Robotics `,
+    link: "",
+    faqs: [
+      {
+        question: "",
+        answer: "",
+      },
+      {
+        question: "",
+        answer: "",
+      },
+    ],
+    rules: [
+      "No materials will be provided from us. You need to bring all the material you need.",
+      "You need to bring the cleanser to clean your face after the event.",
+      "You can use make up or paint on your face",
+      "No plagiarism, otherwise you will be disqualified",
+      "Use of Mobile phones or any other electronic devices is prohibited during the event",
+      "Only 10 mins will be given to arrange your materials.",
+      "Registration for event is only valid through google forms.",
+      "The time duration of the event is 1hr.",
+    ],
+    timeline: "",
+    team: [
+      {
+        name: "Simrella Agrawal (CSE, 6th Sem) ",
+        contact: "7489211170",
+        designation: "Event Managers",
+      },
+    ],
+  },
+  {
+    name: "Exhitbiton and Expo - Decoration",
+    desc: `In this event, the team need to decorate a allocated area inside the campus and area need to be decorated before 30th March`,
+    link: "",
+    faqs: [
+      {
+        question: "",
+        answer: "",
+      },
+      {
+        question: "",
+        answer: "",
+      },
+    ],
+    rules: [
+      "You need to bring all the material you need. We will provide the funds for the materials",
+      "Registration are only valid through google forms.",
+      "Don’t stick cello tapes on walls, otherwise you will be disqualified.",
+      "You will be fined if you misuse the fund or did not decorate the location allotted to you.",
+      "Decoration must be done in groups (minimum: 3 and maximum: 5).",
+      "You can decorate the location of your ideas or themes",
+    ],
+    timeline: "",
+    team: [
+      {
+        name: "Dharashree Patnaik (EE, 6th Sem) ",
+        contact: "8305374964",
+        designation: "Event Managers",
+      },
+    ],
+  },
+  {
+    name: "Exhibiton and Expo - Rangoli",
+    desc: `Participants need to create rangoli on the given theme and the themes are Corona and Meme Templates `,
+    link: "",
+    faqs: [
+      {
+        question: "",
+        answer: "",
+      },
+      {
+        question: "",
+        answer: "",
+      },
+    ],
+    rules: [
+      "Registrations are only valid through google forms.",
+      "Rangoli must be done in groups (minimum: 3 and maximum: 5).",
+      "You can use mobile phone to draw the rangoli.",
+    ],
+    timeline: "",
+    team: [
+      {
+        name: "Rubi Choudhary (CSE, 6th Sem)",
+        contact: "9778741330",
+        designation: "Event Manager",
+      },
+      {
+        name: "Himanshi Singh Chandel(Bsc, 4th sem)",
+        contact: "",
+        designation: "Coordinator",
+      },
+    ],
+  },
+  {
+    name: "Exhibiton and Expo - Colour Painting",
+    desc: `Participants need to paint their painting on the given theme and the themes are Frictional character and Effects of Social Media. `,
+    link: "",
+    faqs: [
+      {
+        question: "",
+        answer: "",
+      },
+      {
+        question: "",
+        answer: "",
+      },
+    ],
+    rules: [
+      "You need to bring your materials except drawing sheet.",
+      "First ten minutes will be given to assemble your things",
+      "Your painting should not be plagiarized.",
+      "Use of electronics devices is prohibited like mobiles, phone and laptops.",
+      "All entries must be submitted through google form.",
+    ],
+    timeline: "",
+    team: [
+      {
+        name: "Rubi Choudhary (CSE, 6th Sem)",
+        contact: "9778741330",
+        designation: "Event Manager",
+      },
+      {
+        name: "Yashvi Solanki(Bcom , 2nd sem)",
+        contact: "",
+        designation: "Coordinator",
+      },
+    ],
+  },
+  {
+    name: "Exhibiton and Expo - OPJU Alumni logo and Flag Design",
+    desc: `In this event you need to design logo and flag related to OP Jindal University`,
+    link: "",
+    faqs: [
+      {
+        question: "",
+        answer: "",
+      },
+      {
+        question: "",
+        answer: "",
+      },
+    ],
+    rules: [
+      "You need to bring your laptops with you.",
+      "Logo and flag must be related to our university.",
+      "Duration of this event is 45min",
+      "Registration for event is only valid through google forms.",
+      "You need to draw both flag and logo.",
+      "According to both design, we will decide the winner",
+    ],
+    timeline: "",
+    team: [
+      {
+        name: "Ajay Sajikumar (CSE, 6th Sem) ",
+        contact: "9109760583",
+        designation: "Event Manager",
+      },
+      {
+        name: "Suyash Dansena(MECH ,4th Sem) ",
+        contact: "9109760583",
+        designation: "Coordinator",
+      },
+    ],
+  },
+  {
+    name: "Exhibition and Expo - Exhibiton",
+    desc: `Exhibition is a public display of works of art or items of interest such as collection of painting, coins, stamps.`,
+    link: "",
+    faqs: [
+      {
+        question: "",
+        answer: "",
+      },
+      {
+        question: "",
+        answer: "",
+      },
+    ],
+    rules: [
+      "Participants need to bring their collection and showcase them in the central library.",
+      "Registration are only valid through google forms.",
+      "Bring your collection before 30th March.",
+    ],
+    timeline: "",
+    team: [
+      {
+        name: "Dharashree Patnaik (EE, 6th Sem) ",
+        contact: "8305374964",
+        designation: "Event Mananger",
+      },
+      {
+        name: "Himanshi Singh Chandel(Bsc, 4th sem)",
+        contact: "",
+        designation: "Coordinaters",
       },
     ],
   },

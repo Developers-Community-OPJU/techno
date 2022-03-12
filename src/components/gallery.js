f99ff34f30e502be6862b2d3c42af559ac766471
Aman Vishwakarma <amankumar430206@gmail.com>
2022-03-12 12:44:01 +0530

    gallery update

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,141 +1,36 @@
 const gallery = [
   {
-    name: "2020",
+    name: "2019",
     gallery: [
       {
         name: "Jubin Nautiyal",
         image: require("@/assets/gallery2020_1.png"),
-        designation: "2020",
+        designation: "2019",
       },
       {
         name: "DJ Olly Esse",
         image: require("@/assets/gallery2020_2.png"),
-        designation: "2020",
+        designation: "2019",
       },
       {
         name: "Solar Car and Cycle",
         image: require("@/assets/gallery2020_3.png"),
-        designation: "2020",
+        designation: "2019",
       },
       {
         name: "Rangoli",
         image: require("@/assets/gallery2020_4.png"),
-        designation: "2020",
+        designation: "2019",
       },
       {
         name: "Scholarship Event",
         image: require("@/assets/gallery2020_5.png"),
-        designation: "2020",
+        designation: "2019",
       },
       {
         name: "VC Speech",
         image: require("@/assets/gallery2020_6.png"),
-        designation: "2020",
-      },
-    ],
-  },
-  {
-    name: "2019",
-    gallery: [
-      {
-        name: "Not provided",
-        image: "",
         designation: "2019",
-      },
-      {
-        name: "Not Provided",
-        image: "",
-        designation: "2019",
-      },
-      {
-        name: "Not provided",
-        image: "",
-        designation: "2019",
-      },
-      {
-        name: "Not provided",
-        image: "",
-        designation: "2019",
-      },
-      {
-        name: "Not provided",
-        image: "",
-        designation: "2019",
-      },
-      {
-        name: "Not provided",
-        image: "",
-        designation: "2019",
-      },
-    ],
-  },
-  {
-    name: "2018",
-    gallery: [
-      {
-        name: "Not Provided",
-        image: "",
-        designation: "2018",
-      },
-      {
-        name: "Not Provided",
-        image: "",
-        designation: "2018",
-      },
-      {
-        name: "Not Provided",
-        image: "",
-        designation: "2018",
-      },
-      {
-        name: "Not Provided",
-        image: "",
-        designation: "2018",
-      },
-      {
-        name: "Not Provided",
-        image: "",
-        designation: "2018",
-      },
-      {
-        name: "Not Provided",
-        image: "",
-        designation: "2018",
-      },
-    ],
-  },
-  {
-    name: "2017",
-    gallery: [
-      {
-        name: "Not Provided",
-        image: "",
-        designation: "2017",
-      },
-      {
-        name: "Not Provided",
-        image: "",
-        designation: "2017",
-      },
-      {
-        name: "Not Provided",
-        image: "",
-        designation: "2017",
-      },
-      {
-        name: "Not Provided",
-        image: "",
-        designation: "2017",
-      },
-      {
-        name: "Not Provided",
-        image: "",
-        designation: "2017",
-      },
-      {
-        name: "Not Provided",
-        image: "",
-        designation: "2017",
       },
     ],
   },

7eeaaf7b2c8056fecf96e670e6a779e3af143187
Utkarsh Dixit <utkarshdixit2021@gmail.com>
2022-03-14 15:08:03 +0530

    Gallery Updated
    2019 to 2020

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,36 +1,36 @@
 const gallery = [
   {
-    name: "2019",
+    name: "2020",
     gallery: [
       {
         name: "Jubin Nautiyal",
         image: require("@/assets/gallery2020_1.png"),
-        designation: "2019",
+        designation: "2020",
       },
       {
         name: "DJ Olly Esse",
         image: require("@/assets/gallery2020_2.png"),
-        designation: "2019",
+        designation: "2020",
       },
       {
         name: "Solar Car and Cycle",
         image: require("@/assets/gallery2020_3.png"),
-        designation: "2019",
+        designation: "2020",
       },
       {
         name: "Rangoli",
         image: require("@/assets/gallery2020_4.png"),
-        designation: "2019",
+        designation: "2020",
       },
       {
         name: "Scholarship Event",
         image: require("@/assets/gallery2020_5.png"),
-        designation: "2019",
+        designation: "2020",
       },
       {
         name: "VC Speech",
         image: require("@/assets/gallery2020_6.png"),
-        designation: "2019",
+        designation: "2020",
       },
     ],
   },

e45536dbdf677b43011c067dbc325d9e6e083709
simrellaagrawal <95346529+simrellaagrawal@users.noreply.github.com>
2022-03-14 16:18:06 +0530

    update scheduledata

--- src/components/schedule.js
+++ src/components/schedule.js
@@ -1,5 +1,6 @@
 const schedule = [
   {
+    day:"1",
     date: "30th March, 2022",
     schedule: [
       {
@@ -8,6 +9,48 @@ const schedule = [
         event: "Techlab : Model Presentation",
       },
       {
+        time: "10:30 AM",
+        venue: "Library",
+        event: "Exhibition and Expo",
+      },
+      {
+        time: "10:00 AM",
+        venue: "Babuji Chowk",
+        event: "Techlab : Model Presentation",
+      },
+      {
+        time: "10:00 AM",
+        venue: "Babuji Chowk",
+        event: "Techlab : Model Presentation",
+      },
+       {
+        time: "10:00 AM",
+        venue: "Babuji Chowk",
+        event: "Techlab : Model Presentation",
+      },
+      {
+        time: "10:00 AM",
+        venue: "Babuji Chowk",
+        event: "Techlab : Model Presentation",
+      },
+      {
+        time: "10:00 AM",
+        venue: "Babuji Chowk",
+        event: "Techlab : Model Presentation",
+      },
+      {
+        time: "10:00 AM",
+        venue: "Babuji Chowk",
+        event: "Techlab : Model Presentation",
+      },
+      
+    ],
+  },
+  {
+    day:"2",
+    date: "31st March, 2022",
+    schedule: [
+      {
         time: "10:00 AM",
         venue: "Babuji Chowk",
         event: "Techlab : Model Presentation",
@@ -15,7 +58,8 @@ const schedule = [
     ],
   },
   {
-    date: "30th March, 2022",
+    day:"3",
+    date: "1st April, 2022",
     schedule: [
       {
         time: "10:00 AM",

473a46d97e848139e92da36c987ec8f2b62649fd
Aman Vishwakarma <amankumar430206@gmail.com>
2022-03-14 15:34:01 +0530

    schedule file format added

---
+++ src/components/schedule.js
@@ -0,0 +1,29 @@
+const schedule = [
+  {
+    date: "30th March, 2022",
+    schedule: [
+      {
+        time: "10:00 AM",
+        venue: "Babuji Chowk",
+        event: "Techlab : Model Presentation",
+      },
+      {
+        time: "10:00 AM",
+        venue: "Babuji Chowk",
+        event: "Techlab : Model Presentation",
+      },
+    ],
+  },
+  {
+    date: "30th March, 2022",
+    schedule: [
+      {
+        time: "10:00 AM",
+        venue: "Babuji Chowk",
+        event: "Techlab : Model Presentation",
+      },
+    ],
+  },
+];
+
+export default schedule;

cc37ac1d266f27d79b29da418791f4196dba20cf
Utkarsh Dixit <utkarshdixit2021@gmail.com>
2022-03-23 15:14:16 +0530

    Dj Perisha Added

--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -126,6 +126,7 @@ export default {
       object-fit: cover;
       object-position: top;
       height: 380px;
+      pointer-events: none;
       filter: grayscale(100%) drop-shadow(1rem 1rem 3rem rgba(44, 44, 44, 0.5));
       transform: scale(1);
     }

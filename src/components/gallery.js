af496cd078a16ff7906c38015bdf6a53a9f27981
Utkarsh Dixit <utkarshdixit2021@gmail.com>
2022-03-29 18:04:51 +0530

    Gallery Images Compressed

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -4,32 +4,32 @@ const gallery = [
     gallery: [
       {
         name: "Jubin Nautiyal",
-        image: require("@/assets/gallery2020_1.png"),
+        image: require("@/assets/gallery2020_1.jpg"),
         designation: "2020",
       },
       {
         name: "DJ Olly Esse",
-        image: require("@/assets/gallery2020_2.png"),
+        image: require("@/assets/gallery2020_2.jpg"),
         designation: "2020",
       },
       {
         name: "Solar Car and Cycle",
-        image: require("@/assets/gallery2020_3.png"),
+        image: require("@/assets/gallery2020_3.jpg"),
         designation: "2020",
       },
       {
         name: "Rangoli",
-        image: require("@/assets/gallery2020_4.png"),
+        image: require("@/assets/gallery2020_4.jpg"),
         designation: "2020",
       },
       {
         name: "Scholarship Event",
-        image: require("@/assets/gallery2020_5.png"),
+        image: require("@/assets/gallery2020_5.jpg"),
         designation: "2020",
       },
       {
         name: "VC Speech",
-        image: require("@/assets/gallery2020_6.png"),
+        image: require("@/assets/gallery2020_6.jpg"),
         designation: "2020",
       },
     ],
